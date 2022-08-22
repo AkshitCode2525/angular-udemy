deba9ae119a8048277f45fd28a7b1fc608900a2f
AkshitCode2525 <akshitcode2525@gmail.com>
2022-08-22 16:22:04 +0530

    ngModel directive modify property value on input field

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,8 @@ import { Component, VERSION } from '@angular/core';
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'akshit thakur';
+export class AppComponent {
+  name = 'Enter you name here';
 }
