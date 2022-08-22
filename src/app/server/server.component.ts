ad23e5a7ef7b10d7982786b1b472cf27fca9bc60
AkshitCode2525 <akshitcode2525@gmail.com>
2022-08-22 23:57:34 +0530

    array of styles

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -3,13 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-server',
   templateUrl: './server.component.html',
-  styles: [
-    `
-  p{
-    color: blue;
-  }
-  `,
-  ],
+  styles: [`p{  color: blue;}`, `h5{ color: green;}`],
 })
 export class ServerComponent implements OnInit {
   constructor() {}
